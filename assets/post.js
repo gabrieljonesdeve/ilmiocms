33be63d97499f1cabce32fae7549031d66971aa4
Gabriel Jones <gabriel.jones@mail.urp.gjdev.it>
2024-09-25 13:59:19 +0200

    Create post.js

---
+++ assets/post.js
@@ -0,0 +1,12 @@
+[
+  {
+    "title": "Primo Post",
+    "date": "2024-09-25",
+    "file": "/blog/posts/post1.md"
+  },
+  {
+    "title": "Secondo Post",
+    "date": "2024-09-26",
+    "file": "/blog/posts/post2.md"
+  }
+]
